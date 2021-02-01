0b45754773910e5e6b40d5ce7115b71fc727aa66
Monika Gapińska <monika.gapinska@lppsa.com>
2021-02-01 22:57:41 +0100

    project update

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,121 +1,103 @@
-import React, { Component } from "react"
-import "./Content.scss"
-import {
-  BrowserRouter as Router,
-  NavLink,
-  Route,
-  Switch,
-} from "react-router-dom"
-import Logo from "../../img/logo.png"
-import Burger from "../../img/burger-4.png"
-import MenuClose from "../../img/menu-close.png"
-import Home from "../Home/Home"
-import AboutUs from "../AboutUs/AboutUs"
-import Offer from "../Offer/Offer"
-import Contact from "../Contact/Contact"
+import React, { Component } from 'react'
+import './Content.scss'
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom'
+import Logo from '../../img/logo.png'
+import Burger from '../../img/burger-4.png'
+import MenuClose from '../../img/menu-close.png'
+import Home from '../Home/Home'
+import AboutUs from '../AboutUs/AboutUs'
+import Offer from '../Offer/Offer'
+import Contact from '../Contact/Contact'
 
-class Header extends Component {
-  state = {
-    toogleMenu: true,
-  }
+class Content extends Component {
+	state = {
+		toogleMenu: true
+	}
 
-  handleToogleMenu = () => {
-    this.setState({
-      toogleMenu: !this.state.toogleMenu,
-    })
-  }
+	handleToogleMenu = () => {
+		this.setState({
+			toogleMenu: !this.state.toogleMenu
+		})
+	}
 
-  render() {
-    const menuStatus = this.state.toogleMenu,
-      manuStatusClass = menuStatus ? "nav" : "nav  active"
+	render() {
+		const menuStatus = this.state.toogleMenu,
+			manuStatusClass = menuStatus ? 'nav' : 'nav  active'
 
-    return (
-      <>
-        <Router>
-          <div className="main-content">
-            <div className="menu-wrapper">
-              <div className="menu">
-                <nav className={manuStatusClass}>
-                  <div className="logo">
-                    <img src={Logo} alt="" />
-                  </div>
-                  <div className="text-logo">TARTAK LORKI</div>
-                  <div
-                    className="burger-nav-wrapper"
-                    onClick={this.handleToogleMenu}
-                  >
-                    <div className="burger-nav">
-                      <img className="menu-open" src={Burger} alt="" />
-                      <img className="menu-close" src={MenuClose} alt="" />
-                    </div>
-                  </div>
-                </nav>
-                <div className="navbar-contact">
-                  <span className="brand-name">TARTAK LORKI</span>
-                  <span className="contact">519 591 509</span>
-                </div>
-              </div>
-            </div>
-            <div
-              className={
-                menuStatus
-                  ? "info-container-wrapper"
-                  : "info-container-wrapper active"
-              }
-            >
-              <ul
-                className={
-                  menuStatus ? "info-container" : "info-container active"
-                }
-              >
-                <li className="list-item">
-                  <NavLink to="/" exact>
-                    Home
-                  </NavLink>
-                </li>
-                <li className="list-item">
-                  <NavLink to="/o-nas">O nas</NavLink>
-                </li>
-                <li className="list-item">
-                  <NavLink to="/produkty">Produkty</NavLink>
-                </li>
-                <li className="list-item">
-                  <NavLink to="/kontakt">Kontakt</NavLink>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/o-nas" component={AboutUs} />
-                <Route path="/produkty" component={Offer} />
-                <Route path="/kontakt" component={Contact} />
-              </Switch>
-            </div>
-          </div>
-          <div className="footer">
-            <div className="menu">
-              <ul className="info-container-1">
-                <li className="list-item">
-                  <NavLink to="/">Home</NavLink>
-                </li>
-                <li className="list-item">
-                  <NavLink to="/o-nas">O nas</NavLink>
-                </li>
-                <li className="list-item">
-                  <NavLink to="/produkty">Produkty</NavLink>
-                </li>
-                <li className="list-item">
-                  <NavLink to="/kontakt">Kontakt</NavLink>
-                </li>
-              </ul>
-            </div>
-            <header className="brand-name">TARTAK LORKI</header>
-          </div>
-        </Router>
-      </>
-    )
-  }
+		return (
+			<div>
+				<Router>
+					<div className="main-content">
+						<div className="menu-wrapper">
+							<div className="menu">
+								<nav className={manuStatusClass}>
+									<div className="logo">
+										<img src={Logo} alt="" />
+									</div>
+									<div className="text-logo">TARTAK LORKI</div>
+									<div className="burger-nav-wrapper" onClick={this.handleToogleMenu}>
+										<div className="burger-nav">
+											<img className="menu-open" src={Burger} alt="" />
+											<img className="menu-close" src={MenuClose} alt="" />
+										</div>
+									</div>
+								</nav>
+								<div className="navbar-contact">
+									<span className="brand-name">TARTAK LORKI</span>
+									<span className="contact">519 591 509</span>
+								</div>
+							</div>
+						</div>
+						<div className={menuStatus ? 'info-container-wrapper' : 'info-container-wrapper active'}>
+							<ul className={menuStatus ? 'info-container' : 'info-container active'}>
+								<li className="list-item">
+									<NavLink to="/" exact>
+										Home
+									</NavLink>
+								</li>
+								<li className="list-item">
+									<NavLink to="/o-nas">O nas</NavLink>
+								</li>
+								<li className="list-item">
+									<NavLink to="/produkty">Produkty</NavLink>
+								</li>
+								<li className="list-item">
+									<NavLink to="/kontakt">Kontakt</NavLink>
+								</li>
+							</ul>
+						</div>
+						<div>
+							<Switch>
+								<Route path="/" exact component={Home} />
+								<Route path="/o-nas" component={AboutUs} />
+								<Route path="/produkty" component={Offer} />
+								<Route path="/kontakt" component={Contact} />
+							</Switch>
+						</div>
+					</div>
+					<div className="footer">
+						<div className="menu">
+							<ul className="info-container-1">
+								<li className="list-item">
+									<NavLink to="/">Home</NavLink>
+								</li>
+								<li className="list-item">
+									<NavLink to="/o-nas">O nas</NavLink>
+								</li>
+								<li className="list-item">
+									<NavLink to="/produkty">Produkty</NavLink>
+								</li>
+								<li className="list-item">
+									<NavLink to="/kontakt">Kontakt</NavLink>
+								</li>
+							</ul>
+						</div>
+						<header className="brand-name">TARTAK LORKI</header>
+					</div>
+				</Router>
+			</div>
+		)
+	}
 }
 
-export default Header
+export default Content
