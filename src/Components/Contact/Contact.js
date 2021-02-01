4c60922d30856a0e6374c193f524eee6cb6980fb
Monika Gapińska <monika.gapinska@lppsa.com>
2021-02-02 00:09:16 +0100

    project update contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,22 +1,34 @@
-import "./Contact.scss"
-import React from "react"
+import './Contact.scss'
+import React from 'react'
 
 const Contact = () => {
-  return (
-    <div className="map-container">
-      <iframe
-        title="Tartak Lorki"
-        width="720"
-        height="400"
-        frameborder="0"
-        scrolling="no"
-        marginheight="0"
-        marginwidth="0"
-        src="https://maps.google.com/maps?width=720&amp;height=400&amp;hl=en&amp;q=Tartak%20Lorki+(Tartak%20Lorki)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-      ></iframe>
-      <a href="https://www.maps.ie/route-planner.htm">Plan A Journey</a>
-    </div>
-  )
+	return (
+		<div className="contact-main-container">
+			<div className="content-data">
+				<h1 className="header">TARTAK LORKI</h1>
+				<p className="data">Lorki 36, 13-324 Grodziczno</p>
+				<p className="data">
+					<span>tel. </span>519-591-509
+				</p>
+				<p className="data">
+					<span>e-mail: </span>biurotartaklorki@gmail.com
+				</p>
+			</div>
+			<div className="map-container">
+				<iframe
+					title="Tartak Lorki"
+					width="720"
+					height="400"
+					frameborder="0"
+					scrolling="no"
+					marginheight="0"
+					marginwidth="0"
+					src="https://maps.google.com/maps?width=720&amp;height=400&amp;hl=en&amp;q=Tartak%20Lorki+(Tartak%20Lorki)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+				/>
+				<a href="https://www.maps.ie/route-planner.htm">Plan A Journey</a>
+			</div>
+		</div>
+	)
 }
 
 export default Contact
