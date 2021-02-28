de64cd2f110e38bcef9d940eb70e963ad9343a21
Krystian Lis <lis.krystian.10@gmail.com>
2021-02-28 12:14:23 +0100

    a small fix

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -25,7 +25,6 @@ const Contact = () => {
 					marginWidth="0"
 					src="https://maps.google.com/maps?width=720&amp;height=400&amp;hl=en&amp;q=Tartak%20Lorki+(Tartak%20Lorki)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
 				/>
-				<a href="https://www.maps.ie/route-planner.htm">Plan A Journey</a>
 			</div>
 		</div>
 	)
